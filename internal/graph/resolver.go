f8734a420fc42c2be1e88472b5063975f201ba17
jeongsikim <jeongsikim@LM-16004496-00.local>
2025-09-09 16:27:46 +0900

    fix graphql test

--- internal/graph/resolver.go
+++ internal/graph/resolver.go
@@ -1,7 +1,9 @@
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+import "github.com/drinkwhale/auto-shopping/internal/graph/model"
 
-type Resolver struct{}
+// Resolver 구조체는 앱의 의존성을 관리합니다.
+// 여기에 todos 슬라이스를 추가하여 임시 데이터베이스로 사용합니다.
+type Resolver struct {
+	todos []*model.Todo
+}
